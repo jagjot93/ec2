fad592c08d343feba497adc520c1096d7c76fc4a
jagjot93 <71182607+jagjot93@users.noreply.github.com>
2022-04-20 17:41:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "myec2" {
-  value=aws_instance.myec2
+  value = "arn  = ${aws_instance.myec2.arn} and private_dns = ${aws_instance.myec2.private_dns}"
 }
+
+/*
+output "myec2" {
+  value = aws_instance.myec2 # this will output all the attributes associated with the aws ec2 instance type
+}
+*/
+
+/*
+output "myec2" {
+  value = aws_instance.myec2.arn # this will output only the 'arn' attribute associated with the aws ec2 instance type
+}
+*/
